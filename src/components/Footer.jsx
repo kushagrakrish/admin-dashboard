0d276571db6bdd2f055cf7fb657a402871da1be4
kushagrakrish <krishna619kk@gmail.com>
2022-06-20 20:13:17 +0530

    yes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <div className='mt-24'>
     <p className='dark:text-gray-200 text-gray-700 text-center m-5 font-semibold'>
-      © 2022 All rights reserved by Shoppy.com
+      © 2022 All rights reserved by Admin-dashboard.com
     </p>
   </div>
 );
